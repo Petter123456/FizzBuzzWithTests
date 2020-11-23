a68c5c00397a543e6362e593d7565ef0cc16d07b
Petter Fagerlund <petter.fagerlund@gmail.com>
2020-11-23 15:34:26 +0100

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+
+    public class Program
+    {
+        public string fizz = "Fizz";
+        public string buzz = "Buzz";
+        public string NumberFortytwo = "Answer to the Ultimate Question of Life, the Universe, and Everything";
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            for (int number = 1; number < 300; number++)
+            {
+                var num = p.FizzBuzz(number);
+                Console.WriteLine(num);
+            }
+        }
+        public string FizzBuzz(int number)
+        {
+            if (number == 42)
+            {
+                return NumberFortytwo;
+            }
+
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                var Fizzbuzz = fizz + buzz.ToLower();
+
+                return Fizzbuzz;
+            }
+
+            if (number % 3 == 0)
+            {
+                return fizz;
+            }
+            else if (number % 5 == 0)
+            {
+                return buzz;
+            }
+            else
+            {
+                return number.ToString();
+            }
+        }
+    }
+}
---
+++ FizzBuzzTests/ProgramTests.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using FluentAssertions;
+
+namespace FizzBuzz.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void If_Number_Is_Dividable_By_3_FizzBuzz_Should_Return_Fizz()
+        {
+            //Arrange
+            Program p = new Program();
+
+            var numbers = new List<int>();
+            for (int number = 1; number < 300; number++)
+            {
+                if (number % 3 == 0)
+                {
+                    numbers.Add(number);
+                }
+            }
+
+            //Act
+            foreach (var number in numbers)
+            {
+                if (number != 42)
+                {
+                    var result = p.FizzBuzz(number);
+
+                    if (number % 3 == 0 && number % 5 == 0)
+                    {
+                        result.Should().BeEquivalentTo("Fizzbuzz");
+                    }
+                    else
+                    {
+                        //Assert
+                        result.Should().BeEquivalentTo("Fizz");
+                    }
+                }
+            }
+        }
+        [TestMethod()]
+        public void If_Number_Is_Dividable_By_5_FizzBuzz_Should_Return_Buzz()
+        {
+            //Arrange
+            Program p = new Program();
+
+            var numbers = new List<int>();
+            for (int number = 1; number < 300; number++)
+            {
+                if (number % 5 == 0)
+                {
+                    numbers.Add(number);
+                }
+            }
+
+            //Act
+            foreach (var number in numbers)
+            {
+                if (number != 42)
+                {
+                    var result = p.FizzBuzz(number);
+
+                    if (number % 3 == 0 && number % 5 == 0)
+                    {
+                        result.Should().BeEquivalentTo("Fizzbuzz");
+                    }
+                    else
+                    {
+                        //Assert
+                        result.Should().BeEquivalentTo("Buzz");
+
+                    }
+                }
+            }
+        }
+        [TestMethod()]
+        public void If_Number_Is_Dividable_By_5_And_3_FizzBuzz_Should_Return_FizzBuzz()
+        {
+            //Arrange
+            Program p = new Program();
+
+            var numbers = new List<int>();
+            for (int number = 1; number < 300; number++)
+            {
+                if (number % 5 == 0 && number % 3 == 0)
+                {
+                    numbers.Add(number);
+                }
+            }
+
+            //Act
+            foreach (var number in numbers)
+            {
+                if (number != 42)
+                {
+                    var result = p.FizzBuzz(number);
+
+                    result.Should().BeEquivalentTo("Fizzbuzz");
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void If_Number_Is_Equal_To_42_FizzBuzz_Should_Return_Answer_to_the_Ultimate_Question_of_Life_the_Universe_and_Everything()
+        {
+            //Arrange
+            Program p = new Program();
+
+            var numbers = new List<int>();
+            for (int number = 1; number < 300; number++)
+            {
+                numbers.Add(number);
+            }
+
+            //Act
+            foreach (var number in numbers)
+            {
+                if (number == 42)
+                {
+                    var result = p.FizzBuzz(number);
+
+                    //Assert
+                    result.Should().BeEquivalentTo("Answer to the Ultimate Question of Life, the Universe, and Everything");
+                }
+            }
+        }
+    }
+}
