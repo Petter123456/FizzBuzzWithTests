5c50cadc99ed5fbb1ab2fb337bb97403e68bb076
Petter Fagerlund <petter.fagerlund@gmail.com>
2020-11-27 12:20:36 +0100

    added some tests, and asks user for input implemented.

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -5,19 +5,48 @@ namespace FizzBuzz
 
     public class Program
     {
+        private static string userInput;
+        private static int number;
         public string fizz = "Fizz";
         public string buzz = "Buzz";
         public string NumberFortytwo = "Answer to the Ultimate Question of Life, the Universe, and Everything";
+        public string question = "Please enter a number between 1 - 300 to run FizzBuzz";
+
         static void Main(string[] args)
         {
             Program p = new Program();
+            Console.WriteLine(p.question);
+           
+            userInput = Console.ReadLine(); 
+            number = p.CheckNumber(userInput); 
 
-            for (int number = 1; number < 300; number++)
+            for (int firstNumber = 1; firstNumber < number; firstNumber++)
             {
-                var num = p.FizzBuzz(number);
+                var num = p.FizzBuzz(firstNumber);
                 Console.WriteLine(num);
             }
         }
+
+        public int CheckNumber(string num)
+        {
+            try
+            {
+               var number = Int32.Parse(num);
+
+                if (number >= 301 || number <= 0)
+                {
+                    Console.WriteLine("The Number you entered was not in range, default value of 300 is added");
+                    return 300; 
+                }
+                return number;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("You entered an invalid number, default value of 300 is added");
+                return 300;
+            }
+        }
+
         public string FizzBuzz(int number)
         {
             if (number == 42)
--- FizzBuzzTests/ProgramTests.cs
+++ FizzBuzzTests/ProgramTests.cs
@@ -127,5 +127,105 @@ namespace FizzBuzz.Tests
                 }
             }
         }
+
+        [TestMethod()]
+        public void FizzBuzz_Should_Return_String()
+        {
+            //Arrange
+            Program p = new Program();
+
+            var numbers = new List<int>();
+            for (int number = 1; number < 300; number++)
+            {
+                numbers.Add(number);
+            }
+
+            //Act
+            foreach (var number in numbers)
+            {
+                    var result = p.FizzBuzz(number);
+
+                    //Assert
+                    result.Should().BeOfType<string>();
+            }
+        }
+
+        [TestMethod()]
+        public void If_No_FizzBuzz_Criteria_Is_Met_FizzBuzz_Should_Return_Number()
+        {
+            //Arrange
+            Program p = new Program();
+
+            var numbers = new List<int>();
+            for (int number = 1; number < 300; number++)
+            {
+                numbers.Add(number);
+            }
+
+            //Act
+            foreach (var number in numbers)
+            {
+                if (number != 42 && number % 3 != 0 && number % 5 != 0)
+                {
+                    var result = p.FizzBuzz(number);
+
+                    //Assert
+                    result.Should().Be(number.ToString());
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void CheckNumber_Should_Return_An_Int()
+        {
+            //Arrange
+            Program p = new Program();
+
+            //Act
+            var sut = p.CheckNumber("500");
+
+            //Assert
+            sut.Should().BeOfType(typeof(int));
+        }
+
+        [TestMethod()]
+        public void CheckNumber_Should_Return_300_if_input_is_not_parsable()
+        {
+            //Arrange
+            Program p = new Program();
+
+            //Act
+            var sut = p.CheckNumber("%%%%");
+
+            //Assert
+            sut.Should().Be(300);
+        }
+
+        [TestMethod()]
+        public void CheckNumber_Should_Return_300_if_input_is_higher_than_300()
+        {
+            //Arrange
+            Program p = new Program();
+
+            //Act
+            var sut = p.CheckNumber("500");
+
+            //Assert
+            sut.Should().Be(300);
+        }
+
+
+        [TestMethod()]
+        public void CheckNumber_Should_Return_300_if_input_is_lower_than_or_equal_to_0()
+        {
+            //Arrange
+            Program p = new Program();
+
+            //Act
+            var sut = p.CheckNumber("-1");
+
+            //Assert
+            sut.Should().Be(300);
+        }
     }
 }
